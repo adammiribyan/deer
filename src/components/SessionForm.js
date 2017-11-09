aeef07fe028a68ce691134f0a790bf0f0ade657b
Adam Miribyan <adam@bysestra.com>
2017-11-09 16:56:58 +0100

    Authenticate and decode the returned token.

--- src/components/SessionForm.js
+++ src/components/SessionForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Env from '../Env';
+import jwt_decode from 'jwt-decode';
 
 export default class SessionForm extends Component {
   constructor(props) {
@@ -10,25 +13,42 @@ export default class SessionForm extends Component {
     }
   }
 
-  onFormSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
+    
+    axios.post('/auth', {
+      user: {
+        email: this.state.email,
+        password: this.state.password
+      }
+    }).then(
+      (response) => {
+        console.log(jwt_decode(response.data.token));
+      }
+    )
   }
 
   render() {
     return(
-      <form onSubmit={this.onFormSubmit}>
-        <div className="form-group">
-          <label for="session_email">Email</label>
-          <input type="email" className="form-control" id="session_email" autofocus="true" />
-        </div>
+      <div className="login-form">
+        <h3 className="login-form__heading">Hey there!</h3>
 
-        <div className="form-group">
-          <label for="session_password">Password</label>
-          <input type="password" className="form-control" id="session_password" />
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="session_email">Email</label>
+            <input type="email" className="form-control" id="session_email" autoFocus="true"
+              onChange={(event) => this.setState({ email: event.target.value })} />
+          </div>
 
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
+          <div className="form-group">
+            <label htmlFor="session_password">Password</label>
+            <input type="password" className="form-control" id="session_password"
+              onChange={(event) => this.setState({ password: event.target.value })} />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Login</button>
+        </form>
+      </div>
     )
   }
 }
